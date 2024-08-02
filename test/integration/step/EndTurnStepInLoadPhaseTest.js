41e6d5e783da3ba1d914184c040075b5be5ae398
Emerson Andrey <emersonandrey_7@hotmail.com>
2024-08-02 09:25:36 -0300

    test: EndTurnStepInLoadPhaseTest

---
+++ test/integration/step/EndTurnStepInLoadPhaseTest.js
@@ -0,0 +1,46 @@
+class EndTurnStepInLoadPhaseTest extends SceneTest {
+  manager = CardBattleManager;
+  step;
+  passed = false;
+
+  create() {
+    const phase = GameConst.LOAD_PHASE;
+    const finish = this.createHandler();
+    const handlers = {
+      playerPlayHandler: () => {},
+      playerPassedHandler: () => {},
+      challengedPlayHandler: () => {},
+      challengedPassedHandler: () => {},
+      activePowerfieldHandler: () => {},
+    };
+    this.step = new TurnStep(this._scene, phase, handlers, finish);
+    this.addAssistedHidden(this.step);
+  }
+
+  start() {
+    this.manager.setPlayerDeck();
+    this.manager.setChallengedDeck();
+    this.manager.playerPassed();
+    this.manager.challengedPassed();
+    this._scene.setStep(this.step);
+    this.step.start(this.manager);
+    this.mockFunction(Input, 'isTriggered', () => true);
+  }
+
+  update() {
+    this.step.update(this.manager);
+  }
+  
+  asserts() {
+    this.describe('Deve apresentar etapa de turno e deve encerrar a fase após jogadas na fase de carregar.');
+    this.expectWasTrue('A janela de tabuleiro do jogador foi apresentado?', this.step.isPlayerBoardWindowVisible);
+    this.expectWasTrue('A janela de batalha do jogador foi apresentada?', this.step.isPlayerBattleWindowVisible);
+    this.expectWasTrue('A janela de pontuação do jogador foi apresentada?', this.step.isPlayerScoreWindowVisible);
+    this.expectWasTrue('A janela de lixo do jogador foi apresentada?', this.step.isPlayerTrashWindowVisible);
+    this.expectWasTrue('A janela de tabuleiro do desafiado foi apresentado?', this.step.isChallengedBoardWindowVisible);
+    this.expectWasTrue('A janela de batalha do desafiado foi apresentada?', this.step.isChallengedBattleWindowVisible);
+    this.expectWasTrue('A janela de pontuação do desafiado foi apresentada?', this.step.isChallengedScoreWindowVisible);
+    this.expectWasTrue('A janela de lixo do desafiado foi apresentada?', this.step.isChallengedTrashWindowVisible);
+    this.expectTrue('A proxima Etapa é DisplayStep?', this.isStep(DisplayStep));
+  }
+}
