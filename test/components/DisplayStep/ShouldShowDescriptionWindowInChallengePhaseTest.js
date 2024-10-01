2ea532069ebe593018d48747e933277576fe478f
Emerson Andrey <emersonandrey_7@hotmail.com>
2024-09-30 22:41:38 -0300

    tests: ShouldShowDescriptionWindowInChallengePhaseTest

---
+++ test/components/DisplayStep/ShouldShowDescriptionWindowInChallengePhaseTest.js
@@ -0,0 +1,30 @@
+class ShouldShowDescriptionWindowInChallengePhaseTest extends SceneTest {
+  step;
+
+  create() {
+    this.createHandler();
+    this.step = new DisplayStep(this._scene, GameConst.CHALLENGE_PHASE);
+    this.addAssistedHidden(this.step);
+  }
+
+  start() {
+    this.setStep(this.step);
+    this.step.start();
+    const finish = this.getHandler();
+    this.mockFunction(Input, 'isTriggered', finish);
+  }
+
+  update() {
+    this.step.update();
+  }
+  
+  asserts() {
+    this.describe('Deve apresentar janela de descrição em etapa de apresentação de fase de desafio.');
+    this.expectWasTrue('A janela de descrição foi apresentada?', this.step.isDescriptionWindowVisible);
+    const texts = [
+      'Descrição de Desafiado',
+      'O jogador que é desafiado por você.',
+    ];
+    this.expectTrue('A descrição da fase foi apresentada como?', this.step.isTextDescriptionWindow(texts));
+  }
+}
