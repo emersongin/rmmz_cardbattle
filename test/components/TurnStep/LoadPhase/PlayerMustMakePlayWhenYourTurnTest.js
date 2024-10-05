488939968118f91bca3286e0f75834d73d36a406
Emerson Andrey <emersonandrey_7@hotmail.com>
2024-10-05 14:19:51 -0300

    test: PlayerMustMakePlayWhenYourTurnLoadPhaseTest

---
+++ test/components/TurnStep/LoadPhase/PlayerMustMakePlayWhenYourTurnTest.js
@@ -0,0 +1,72 @@
+class PlayerMustMakePlayWhenYourTurnLoadPhaseTest extends SceneTest {
+  step;
+
+  create() {
+    const finish = this.createHandler();
+    const handlers = {
+      playerPlayHandler: () => {
+        const config = {
+          location: GameConst.HAND,
+          player: GameConst.PLAYER,
+          blockBattleCards: true,
+          blockPowerCardsInLoadPhase: true,
+        };
+        const handlers = {
+          goBackHandler: () => {},
+          selectHandler: () => {},
+          moveCursorHandler: () => {},
+        };
+        this.step.changeStep(ZoneStep, config, handlers);
+        finish();
+      },
+      playerPassedHandler: () => {},
+      challengedPlayHandler: () => {},
+      challengedPassedHandler: () => {},
+      activePowerfieldHandler: () => {},
+    };
+    this.step = new TurnStep(this._scene, GameConst.LOAD_PHASE, handlers);
+    this.addAssistedHidden(this.step);
+  }
+
+  start() {
+    CardBattleManager.folders[0] = {
+      name: 'Mock Folder',
+      energies: [0, 0, 0, 0, 0, 0],
+      set: [
+        { type: GameConst.BATTLE, color: GameConst.RED, figureName: 'default', attack: 10, health: 10, isActiveInLoadPhase: false },
+        { type: GameConst.BATTLE, color: GameConst.BLUE, figureName: 'default', attack: 10, health: 10, isActiveInLoadPhase: false },
+        { type: GameConst.BATTLE, color: GameConst.WHITE, figureName: 'default', attack: 10, health: 10, isActiveInLoadPhase: false },
+        { type: GameConst.POWER, color: GameConst.GREEN, figureName: 'default', attack: 10, health: 10, isActiveInLoadPhase: true },
+        { type: GameConst.POWER, color: GameConst.BLACK, figureName: 'default', attack: 10, health: 10, isActiveInLoadPhase: true },
+        { type: GameConst.POWER, color: GameConst.BROWN, figureName: 'default', attack: 10, health: 10, isActiveInLoadPhase: true },
+      ]
+    };
+    CardBattleManager.setPlayerDeck(0);
+    CardBattleManager.setChallengedDeck(0);
+    const drawNumber = 6;
+    const putNumber = 3;
+    CardBattleManager.drawPlayerCards(drawNumber);
+    CardBattleManager.putPlayerCards(putNumber);
+    CardBattleManager.drawChallengedCards(drawNumber);
+    CardBattleManager.putChallengedCards(putNumber);
+    CardBattleManager.playerStart();
+    this.mockFunction(Input, 'isTriggered', () => true);
+    this.spyFunction(this.step, 'commandOpenAskWindow', () => {
+      const index = 0;
+      this.step.selectAskWindowOption(index);
+    });
+    this._scene.setStep(this.step);
+    this.step.start();
+  }
+
+  update() {
+    this.step.update();
+  }
+  
+  asserts() {
+    this.describe('Jogador deve fazer uma jogada quando for sua vez em fase de carregamento.');
+    this.expectWasTrue('A janela de decisão foi aberta?', this.step.isAskWindowVisible);
+    this.expectTrue('Jogador tem cartões de poder para jogar?', CardBattleManager.isPlayerHasPowerCardInHand());
+    this.expectTrue('A proxima etapa é ZoneStep?', this.isStep(ZoneStep));
+  }
+}
